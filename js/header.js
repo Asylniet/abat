7c81ba74557a11ec6d1b023485622f2276c7534b
Asylniet <zasylniet@mail.ru>
2022-09-13 22:14:55 +0600

    language change button

--- js/header.js
+++ js/header.js
@@ -23,6 +23,15 @@ class Header extends HTMLElement {
             <img src="/assets/icons/call.svg" alt="Контакты" />
             <a href="tel:+7 771 100 55 77">+7 771 100 55 77</a>
           </div>
+          <div class="dropdown-menu fs-400 fw-medium">
+              <div class="text-holder"></div>
+              <img src="/assets/icons/arrow.svg" class="dropdown-arrow">
+              <div class="options">
+                <div class="option active">RU</div>
+                <div class="option">KZ</div>
+                <div class="option">EN</div>
+              </div>
+          </div>
           <div class="hamburger">
             <span></span>
             <span></span>
@@ -32,7 +41,7 @@ class Header extends HTMLElement {
       </div>
       <nav class="flex">
         <div class="container">
-          <ul role="list">
+          <ul role="list" class="list-holder">
             <a href="/" class="${path === "index" && "selected"}">О компании</a>
             <a href="/pages/services.html" class="${
               path === "services" && "selected"
@@ -50,8 +59,8 @@ class Header extends HTMLElement {
               path === "clients" && "selected"
             }">Клиенты</a>
             <a href="/pages/contacts.html" class="${
-                path === "contacts" && "selected"
-              }">Контакты</a>
+              path === "contacts" && "selected"
+            }">Контакты</a>
           </ul>
         </div>
       </nav>
@@ -60,6 +69,11 @@ class Header extends HTMLElement {
     const body = document.querySelector("body");
     const header = document.querySelector("header");
     const hamburger = document.querySelector(".hamburger");
+
+    const dropdown = document.querySelector(".dropdown-menu");
+    const textHolder = document.querySelector(".text-holder");
+    const options = document.querySelectorAll(".option");
+    const arrowImg = document.querySelector(".dropdown-arrow");
 
     window.addEventListener("load", () => {
       hamburger.addEventListener("click", () => {
@@ -70,6 +84,36 @@ class Header extends HTMLElement {
           header.classList.add("opened");
           body.classList.add("non-scrollable");
         }
+      });
+
+      if(window.innerWidth <= 1125) {
+        document.querySelector(".list-holder").appendChild(dropdown);
+        arrowImg.setAttribute('src', '/assets/icons/arrow-white.svg')
+      }
+
+      textHolder.innerHTML = document.querySelector(".option.active").innerHTML;
+
+      function openDropDown() {
+        dropdown.classList.contains("open")
+          ? dropdown.classList.remove("open")
+          : dropdown.classList.add("open");
+      }
+      textHolder.addEventListener("click", () => {
+        openDropDown();
+      });
+      arrowImg.addEventListener("click", () => {
+        openDropDown();
+      });
+
+      options.forEach((el) => {
+        el.addEventListener("click", () => {
+          options.forEach((el1) => {
+            el1.classList.remove("active");
+          });
+          el.classList.add("active");
+          textHolder.innerHTML = el.innerHTML;
+          openDropDown();
+        });
       });
     });
   }
